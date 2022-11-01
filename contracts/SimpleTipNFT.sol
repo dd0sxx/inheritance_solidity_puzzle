e4cc869ef7f951096cbfe8af555d081a624bd7bd
dd0sxx <contacttheotelonis@gmail.com>
2022-11-01 16:23:23 -0600

    added voting power

--- contracts/SimpleTipNFT.sol
+++ contracts/SimpleTipNFT.sol
@@ -5,22 +5,52 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 import "solmate/src/tokens/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleTipNFT is ERC721 {
-    address owner;
+contract SimpleTipNFT is ERC721, Ownable {
 
+    uint256 currentSupply;
+    mapping (address => uint256) votingPower; // 1 NFT = 1 vote
 
-    constructor() ERC721("Simple Tip NFT", "ST") {
-        owner = msg.sender;
-    }
+    constructor() ERC721("Simple Tip NFT", "ST") {}
 
     function mint() public payable {
         uint256 _currentSupply = currentSupply;
-        require(msg.value == 0.01 ether && _currentSupply < 10000);
+        require(msg.value > 0.01 ether && _currentSupply < 10000);
         _mint(msg.sender, _currentSupply);
         unchecked {
             currentSupply++;
+            votingPower[msg.sender]++;
         }
+    }
+
+    function transferFrom (
+        address from,
+        address to,
+        uint256 id
+    ) public override {
+        transferVotingPower(from, to);
+        super.transferFrom(from, to, id);
+    }
+
+    function safeTransferFrom (
+        address from,
+        address to,
+        uint256 id
+    ) public override {
+        transferVotingPower(from, to);
+        super.safeTransferFrom(from, to, id);
+    }
+
+    function transferVotingPower (address from, address to) internal {
+        unchecked {
+
+        }
+    }
+
+    function withdraw () external onlyOwner {
+        (bool success, bytes memory response) = owner().call{value: address(this).balance}("");
+        require(success);
     }
 
     function tokenURI(uint256 id)
@@ -30,30 +60,13 @@ contract SimpleTipNFT is ERC721 {
         override
         returns (string memory)
     {
+        return string(abi.encodePacked("ipfs://deadbeef/", id));
+    }
+
+    receive () external payable {
 
     }
 
-    function transferFrom (
-        address from,
-        address to,
-        uint256 id
-    ) public payable override {
-        doSomething();
-        super.transferFrom(from, to, id);
-    }
 
-    function safeTransferFrom (
-        address from,
-        address to,
-        uint256 id
-    ) public payable override {
-        require(msg.value > 0, "INVALID_TIP");
-        doSomething();
-        super.safeTransferFrom(from, to, id);
-    }
-
-    function doSomething () internal {
-
-    }
 
 }
